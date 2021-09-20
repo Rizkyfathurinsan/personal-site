f5391cfd655e13a5c3ca3e30e955d95a37c17b80
Rizkyfathurinsan <rizkyfathurinsan@gmail.com>
2021-09-20 20:34:54 +0700

    webpack configurated to contain 2 of the main css styles.

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,8 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/style.scss';
+import './styles/parallax.scss';
 
 // start the Stimulus application
 import './bootstrap';
